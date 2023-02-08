6ec45fba7e557f5a72e2b90032ae82446e5b6b43
markischengg <markischeng28@uchicago.edu>
2023-02-08 03:26:59 +0000

    v2

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,14 @@ class DirectorsController < ApplicationController
     @list_of_directors = Director.all
     render({ :template =>  "directors_template/index.html.erb"})
   end
+
+  def eldest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "directors_template/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.order({ :dob => :desc }).at(0)
+    render({ :template => "directors_templates/youngest.html.erb"})
+  end
 end
